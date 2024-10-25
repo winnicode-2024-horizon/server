aab677466341b13ba4e2cc58775646b1d9014a53
actiangent <hisyam.fausta@gmail.com>
2024-10-25 14:57:59 +0700

    First commit

---
+++ src/main/java/id/winnicode/horizon/HorizonApplication.java
@@ -0,0 +1,12 @@
+package id.winnicode.horizon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorizonApplication.class, args);
+    }
+}
